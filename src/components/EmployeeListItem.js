a5cea66ba5dcf9dd7344e314080ff63ae5f575b2
harsha973 <harsha973@gmail.com>
2018-12-20 11:42:59 +1300

    Styling changes for employee row

--- src/components/EmployeeListItem.js
+++ src/components/EmployeeListItem.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { Text, TouchableOpacity } from 'react-native';
 import { Actions } from 'react-native-router-flux';
+import { CardSection } from './common';
 
 class EmployeeListItem extends Component {
 
@@ -12,9 +13,9 @@ class EmployeeListItem extends Component {
       const employee = this.props.item;
       return (
         <TouchableOpacity onPress={this.rowClicked.bind(this)}>
-          <View style={styles.containerStyle}>
+          <CardSection>
             <Text style={styles.textStyle}>{employee.name}</Text>
-          </View>
+          </CardSection>
         </TouchableOpacity>
       );
   }
@@ -23,14 +24,7 @@ class EmployeeListItem extends Component {
 const styles = {
   textStyle: {
     fontSize: 18,
-    paddingLeft: 15,
-    paddingTop: 5,
-    paddingBottom: 5
-  },
-  containerStyle: {
-    backgroundColor: 'white',
-    borderBottomWidth: 1,
-    borderColor: '#ddd'
+    paddingLeft: 15
   }
 };
 
